4a9ed95656f715fc110b2452449a4e07f23999bb
Ashlotl <thesupremeliverwurst@gmail.com>
2023-10-03 11:04:39 -0700

    hopefully final commit, i am realizing how unnecessary this all is retroactively

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     collections::HashMap,
-    fs,
+    env, fs,
     io::{self, ErrorKind},
     net::UdpSocket,
     sync::{
@@ -75,7 +75,7 @@ fn get_next_data(socket: &UdpSocket) -> Result<Option<SensorBatch>, io::Error> {
     Ok(ret)
 }
 
-fn save_file(csv: &Vec<Vec<String>>) {
+fn save_file(csv: &Vec<Vec<String>>, path: &str) {
     let mut csv_bytes = vec![];
     for row in 0..csv[0].len() {
         for column in 0..csv.len() {
@@ -90,10 +90,129 @@ fn save_file(csv: &Vec<Vec<String>>) {
         csv_bytes.push('\n' as u8);
     }
 
-    fs::write("output.csv", csv_bytes).unwrap();
+    fs::write(path, csv_bytes).unwrap();
+}
+
+fn aggregate() {
+    let contents = fs::read_to_string("output.csv").unwrap();
+    //fill array
+    let mut csv = vec![];
+    let lines: Vec<_> = contents.split('\n').collect();
+    let row0: Vec<_> = lines[0].split(',').collect();
+    for column_i in 0..row0.len() {
+        csv.push(vec![]);
+        for row_i in 0..lines.len() {
+            let row: Vec<_> = lines[row_i].split(',').collect();
+            if row.len() == 1 || row[column_i] == "" {
+                continue;
+            }
+            csv[column_i].push(row[column_i]);
+        }
+    }
+
+    //do the actual aggregation
+    //find the smallest time range
+    let mut smallest_time_range = str::parse::<f64>(csv[0][csv[0].len() - 1]).unwrap()
+        - str::parse::<f64>(csv[0][1]).unwrap();
+    let mut smallest_time_range_i5th = 0;
+    for column_i5th in 1..csv.len() / 5 {
+        let column_i = column_i5th * 5;
+        let range = str::parse::<f64>(csv[column_i][csv[column_i].len() - 1]).unwrap()
+            - str::parse::<f64>(csv[column_i][1]).unwrap();
+        if range < smallest_time_range {
+            smallest_time_range_i5th = column_i5th;
+            smallest_time_range = range;
+        }
+    }
+
+    let mut target_csv: Vec<Vec<f64>> = (0..csv.len() / 5 * 3 + 1)
+        .map(|_| {
+            (0..csv[smallest_time_range_i5th * 5].len())
+                .map(|_| 0.0)
+                .collect()
+        })
+        .collect();
+    //iterate through column with smallest time range
+    for row_target in 1..csv[smallest_time_range_i5th * 5].len() {
+        let target_timestamp: f64 =
+            str::parse(csv[smallest_time_range_i5th * 5][row_target]).unwrap();
+        target_csv[0][row_target - 1] = target_timestamp;
+        let prev_timestamp = if row_target > 1 {
+            str::parse(csv[smallest_time_range_i5th * 5][row_target - 1]).unwrap()
+        } else {
+            target_timestamp
+        };
+        let next_timestamp = if row_target < csv[smallest_time_range_i5th * 5].len() - 1 {
+            str::parse(csv[smallest_time_range_i5th * 5][row_target + 1]).unwrap()
+        } else {
+            target_timestamp
+        };
+        let lower_bound = (prev_timestamp + target_timestamp) / 2.0;
+        let upper_bound = (next_timestamp + target_timestamp) / 2.0;
+
+        for column_i5th in 0..csv.len() / 5 {
+            let mut sum_x = 0.0;
+            let mut sum_y = 0.0;
+            let mut sum_z = 0.0;
+            let mut counted = 0;
+
+            for row_i in 1..csv[column_i5th * 5].len() {
+                let timestamp: f64 = str::parse(csv[column_i5th * 5][row_i]).unwrap();
+                if timestamp == target_timestamp
+                    || (timestamp >= lower_bound && timestamp < upper_bound)
+                {
+                    sum_x += str::parse::<f64>(csv[column_i5th * 5 + 1][row_i]).unwrap();
+                    sum_y += str::parse::<f64>(csv[column_i5th * 5 + 2][row_i]).unwrap();
+                    sum_z += str::parse::<f64>(csv[column_i5th * 5 + 3][row_i]).unwrap();
+                    counted += 1;
+                } else if timestamp > upper_bound {
+                    break;
+                }
+            }
+
+            if counted == 0 {
+                if row_target > 2 {
+                    target_csv[column_i5th * 3 + 1][row_target - 1] =
+                        target_csv[column_i5th * 3 + 1][row_target - 2];
+                    target_csv[column_i5th * 3 + 2][row_target - 1] =
+                        target_csv[column_i5th * 3 + 2][row_target - 2];
+                    target_csv[column_i5th * 3 + 3][row_target - 1] =
+                        target_csv[column_i5th * 3 + 3][row_target - 2];
+                }
+                continue;
+            }
+
+            let average_x = sum_x / counted as f64;
+            let average_y = sum_y / counted as f64;
+            let average_z = sum_z / counted as f64;
+            target_csv[column_i5th * 3 + 1][row_target - 1] = average_x;
+            target_csv[column_i5th * 3 + 2][row_target - 1] = average_y;
+            target_csv[column_i5th * 3 + 3][row_target - 1] = average_z;
+        }
+    }
+
+    let mut csv_bytes = vec![];
+    csv_bytes.extend_from_slice(lines[0].replace(",Time (s),", "").as_bytes());
+    csv_bytes.push('\n' as u8);
+    for row in 0..target_csv[0].len() {
+        for column in 0..target_csv.len() {
+            let value = format!("{},", target_csv[column][row]);
+            let cell_bytes = value.as_bytes();
+            csv_bytes.extend_from_slice(cell_bytes);
+        }
+        csv_bytes.push('\n' as u8);
+    }
+
+    fs::write("output_aggregated.csv", csv_bytes).unwrap();
 }
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 && args[1] == "aggregate" {
+        aggregate();
+        return;
+    }
+
     let socket = UdpSocket::bind("0.0.0.0:5555").unwrap();
     socket
         .set_read_timeout(Some(Duration::from_secs(5)))
@@ -130,7 +249,7 @@ fn main() {
     let (file_read_done_tx, file_read_done_rx) = mpsc::channel();
     let ctrl_c_quit = quit.clone();
     ctrlc::set_handler(move || {
-        save_file(&ctrl_c_output_csv.lock().unwrap());
+        save_file(&ctrl_c_output_csv.lock().unwrap(), "output.csv");
         println!("Quitting... This should not take longer than 5 seconds.");
         ctrl_c_quit.store(true, Ordering::SeqCst);
         file_read_done_tx.send(()).unwrap();
